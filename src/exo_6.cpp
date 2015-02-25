f56de11c2822704c079cea46dbb1276ce414a5b6
drew1d <andrelo1d@hotmail.com>
2015-02-25 16:36:44 -0500

    Adding solver for TDS

---
+++ src/exo_6.cpp
@@ -0,0 +1,51 @@
+/*__________________________________________________________________________________________________
+
+  autor: Pedro Guarderas
+  email: ajusworkopensource@gmail.com
+  date: 25-02-2015
+  file: exo_6.cpp
+  This program is free software; you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software Foundation;
+  either version 2 of the License, or (at your option) any later version.
+  __________________________________________________________________________________________________
+*/
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include "solver.hpp"
+
+using namespace std;
+
+double f( double t ) {
+  return( sin(3.14159 * t ) * cos( 20 * 3.14159 * t )  ); 
+}
+
+int main() {
+	double n = 1000;
+	double T = 20.0, h;
+	vector< double > t( n ), F( n ), a( n ), b( n ), c( n ); 
+	h = T / n;
+	
+	for ( size_t i = 0; i < n; i++ ) {
+	  t[i] = i * h;
+	  F[i] = f( t[i] );
+	  a[i] = 1.0 / pow( h, 2 );
+	  b[i] = -2.0 / pow( h, 2 );
+	  c[i] = 1.0 / pow( h, 2 );
+	}
+
+	solveTDS( a, b, c, F );
+
+	for( size_t i = 0; i < n; i++ ) {
+		cout << t[i] << "\t" << F[i] << endl;
+	}
+
+	return 0;
+}
+
+/* Gráfico en R
+ * s<-read.table( '/home/drew/Development/CFINI/src/output.txt')
+   plot( s[,1], s[,2], col = 'red', cex = 0.5, type = 'l' ) */
--- src/solver.hpp
+++ src/solver.hpp
@@ -10,30 +10,26 @@
   __________________________________________________________________________________________________
 */
 
-
-#include <iostream>
 #include <vector>
-
-#include "solver.hpp"
 
 using namespace std;
 
+void solveTDS( const vector< double > a, const vector< double > b,
+            vector< double >& c, vector< double >& d ) {
 
-int main() {
-	vector< double > a = { 0, -1, -1, -1 };
-	vector< double > b = { 4,  4,  4,  4 };
-	vector< double > c = { -1, -1, -1,  0 };
-	vector< double > d = { 5,  5, 10, 23 };
-	
-	solveTDS( a, b, c, d );
+	size_t n = a.size();
+	c[0] /= b[0];
+	d[0] /= b[0];
+	n--;
 
-	for( size_t i = 0; i < a.size(); i++ ) {
-		cout << d[i] << endl;
+	for( size_t i = 1; i < n; i++ ) {
+		c[i] /= b[i] - a[i] * c[i - 1];
+		d[i] = ( d[i] - a[i] * d[i - 1] ) / ( b[i] - a[i] * c[i - 1] );
 	}
 
-	return 0;
+	d[n] = ( d[n] - a[n] * d[n - 1] ) / ( b[n] - a[n] * c[n - 1] );
+
+	for( size_t i = n; i-- > 0; ) {
+		d[i] -= c[i] * d[i + 1];
+	}
 }
-
-
-
-
