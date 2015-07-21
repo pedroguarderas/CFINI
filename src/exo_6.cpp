9a40c81e084a35e824c2d38813ffa516f898dc92
aju <ajusan@te.ceaaces>
2015-07-20 20:21:47 -0500

    Mejorando ejemplo

--- src/exo_6.cpp
+++ src/exo_6.cpp
@@ -23,46 +23,46 @@ using namespace std;
  * Solving second order differential equation
  */
 double f( double t ) {
-   return( 100 * sin(3.14159 * t ) * cos( 20 * 3.14159 * t ) + 0.5 * t ); 
+	return( 100 * sin( 3.14159 * t ) * cos( 20 * 3.14159 * t ) + 0.5 * t );
 }
 
 int main() {
-	double n = 1000;
+	double n = 10000;
 	double T0 = 0.0, T1 = 2.0, h;
-  double alpha, beta;
-	vector< double > t( n + 2 ), F( n ), a( n ), b( n ), c( n ); 
-  
-  alpha = 0.0;
-  beta = 1.0;
+	double alpha, beta;
+	vector< double > t( n + 2 ), F( n ), a( n ), b( n ), c( n );
+
+	alpha = 0.5;
+	beta = 1.0;
 	h = ( T1 - T0 ) / ( n + 1 );
-	
-  t[0] = T0;
-	for ( size_t i = 0; i < n; i++ ) {
-	  t[i+1] = T0 + (i+1) * h;
-	  F[i] = f( t[i+1] );
-	  a[i] = 1.0 / pow( h, 2 );
-	  b[i] = -2.0 / pow( h, 2 );
-	  c[i] = 1.0 / pow( h, 2 );
+
+	t[0] = T0;
+	for( size_t i = 0; i < n; i++ ) {
+		t[i + 1] = T0 + ( i + 1 ) * h;
+		F[i] = f( t[i + 1] );
+		a[i] = 1.0 / pow( h, 2 );
+		b[i] = -2.0 / pow( h, 2 );
+		c[i] = 1.0 / pow( h, 2 );
 	}
-	t[n+1] = T1;
-  F[0] -= alpha / pow( h, 2 );
-  F[n-1] -= beta / pow( h, 2 );
+	t[n + 1] = T1;
+	F[0] -= alpha / pow( h, 2 );
+	F[n - 1] -= beta / pow( h, 2 );
 
 	solveTDS( a, b, c, F );
 
-  ofstream file;
-  file.open("output.txt");
-  file << t[0] << "\t" << alpha << endl;
+	ofstream file;
+	file.open( "output.txt" );
+	file << t[0] << "\t" << alpha << endl;
 	for( size_t i = 0; i < n; i++ ) {
-		file << t[i+1] << "\t" << F[i] << endl;
+		file << t[i + 1] << "\t" << F[i] << endl;
 	}
-	file << t[n+1] << "\t" << beta << endl;
-  file.close();
+	file << t[n + 1] << "\t" << beta << endl;
+	file.close();
 
 	return 0;
 }
 
 /* Gráfico en R
  s<-read.table( '[path]/CFINI/src/output.txt')
- plot( s[,1], s[,2], col = 'red', cex = 0.5, type = 'l', xlab = 't', ylab = 'u' ) 
+ plot( s[,1], s[,2], col = 'red', cex = 0.5, type = 'l', xlab = 't', ylab = 'u' )
  */
