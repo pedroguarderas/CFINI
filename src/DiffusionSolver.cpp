856ededa6a3203db1486a390fe4bd2ef5eb3f545
pedroguarderas <pedro.guarderas@webrisko.com>
2017-08-18 17:13:32 -0500

    Tridiagonal Solver in separated source file

--- src/DiffusionSolver.cpp
+++ src/DiffusionSolver.cpp
@@ -50,36 +50,6 @@ List DiffusionSolverES( const double& alpha,
 }
 
 // [[Rcpp::export]]
-void TriDiagSolver( arma::colvec& a,
-                    arma::colvec& b,
-                    arma::colvec& c,
-                    arma::colvec& d ) {
-
-  double mu;
-  int N = d.size();
-  int i;
-  
-  N--; 
-  c(0) /= b(0);
-  d(0) /= b(0);
-  
-  // Forward recursion
-  for ( i = 1; i < N; i++ ) {
-    c(i) /= b( i ) - a( i ) * c( i - 1 );
-    d( i ) = ( d( i ) - a( i ) * d( i - 1 ) ) / ( b( i ) - a( i ) * c( i - 1 ) );
-  }
-  
-  d( N ) = ( d( N ) - a( N ) * d( N - 1 ) ) / ( b( N ) - a( N ) * c( N - 1 ) );
-  
-  // Backsubstitution
-  for ( i = N; i-- > 0; ) {
-    d( i ) -= c( i ) * d( i + 1 );
-  }
-
-}
-
-
-// [[Rcpp::export]]
 List DiffusionSolverCNS( const double& alpha,
                          const double& theta,
                          const arma::colvec& I,
---
+++ src/TriDiagSolver.cpp
@@ -0,0 +1,35 @@
+#include <RcppArmadillo.h>
+
+using namespace Rcpp;
+
+//[[Rcpp::plugins(cpp14)]]
+// [[Rcpp::depends(RcppArmadillo)]]
+
+// [[Rcpp::export]]
+void TriDiagSolver( arma::colvec& a,
+                    arma::colvec& b,
+                    arma::colvec& c,
+                    arma::colvec& d ) {
+  
+  double mu;
+  int N = d.size();
+  int i;
+  
+  N--; 
+  c(0) /= b(0);
+  d(0) /= b(0);
+  
+  // Forward recursion
+  for ( i = 1; i < N; i++ ) {
+    c(i) /= b( i ) - a( i ) * c( i - 1 );
+    d( i ) = ( d( i ) - a( i ) * d( i - 1 ) ) / ( b( i ) - a( i ) * c( i - 1 ) );
+  }
+  
+  d( N ) = ( d( N ) - a( N ) * d( N - 1 ) ) / ( b( N ) - a( N ) * c( N - 1 ) );
+  
+  // Backsubstitution
+  for ( i = N; i-- > 0; ) {
+    d( i ) -= c( i ) * d( i + 1 );
+  }
+  
+}
