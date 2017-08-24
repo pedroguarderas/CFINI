05d07cdce71d5ed3aac736b970eeaed6a319b8f6
pedroguarderas <pedro.guarderas@webrisko.com>
2017-08-23 21:27:09 -0500

    Testing another escenario

--- tests/example_4.R
+++ tests/example_4.R
@@ -3,7 +3,7 @@ library( plotly )
 library( Rcpp )
 library( RcppArmadillo )
 
-sigma<-0.7
+sigma<-1
 r<-0.05
 alpha<-( sigma^2 - 2 * r ) / ( 2 * sigma^2 )
 beta<- -( ( 2 * r + sigma^2 ) / ( 2 * sigma^2 ) )^2
@@ -11,14 +11,14 @@ beta<- -( ( 2 * r + sigma^2 ) / ( 2 * sigma^2 ) )^2
 t0<-0
 t1<-1
 t1<-t1 * ( sigma^2 ) / 2
-Nt<-300
-t<-GridUniform( t0, t1, Nt )
-# t<-GridExpAddapt( -3, t0, t1, Nt, 1 )
+Nt<-5000
+# t<-GridUniform( t0, t1, Nt )
+t<-GridExpAddapt( 1, t0, t1, Nt, 1 )
 
 x0<--8
 x1<-8
-Nx<-100
-x<-GridExpAddapt( -4, x0, x1, Nx, 1 )
+Nx<-300
+x<-GridExpAddapt( -4.5, x0, x1, Nx, 1 )
 
 K<-1500
 I<-sapply( x, FUN = function( x ) exp( -alpha * x ) * max( exp( x ) - K, 0 ) )
@@ -41,8 +41,8 @@ V<-CFBlackScholesSolvCNS( sigma, r, theta, I, A, B, t, x )
 # plot_ly( x = V$t[,1], y = V$x[,1], z = V$u, alpha = 0.8 ) %>% add_surface()
 
 # View( V$u )
-persp( x = V$t[,1], y = V$x[,1], z = V$u, col = 'red',
-       theta = 40, xlab = 't', ylab = 'S', zlab = 'V' )
+# persp( x = V$t[,1], y = V$x[,1], z = V$u, col = 'red',
+       # theta = 40, xlab = 't', ylab = 'S', zlab = 'V' )
 # persp( t, S, u )
 
 plot( V$x[,1], V$u[Nt,], type = 'l', col = 'red',
@@ -50,7 +50,7 @@ plot( V$x[,1], V$u[Nt,], type = 'l', col = 'red',
       xlim = c( 0, 3000 ), ylim = c( 0, 1750 ), axes = FALSE )
 axis( 1, at=seq( 0, 3000, 250 ), labels=seq( 0, 3000, 250 ) )
 axis( 2, at=seq( 0, 1750, 250 ), labels=seq( 0, 1750, 250 ) )
-for ( n in seq( Nt-1, 1, -5 ) ) {
+for ( n in seq( Nt-1, 1, -100 ) ) {
   points( V$x[,1], V$u[n,], type = 'l', col = 'red',
         xlab = 'S', ylab = 'u',
         xlim = c( 0, 3000 ), ylim = c( 0, 1750 ) )
