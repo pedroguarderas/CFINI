06b81d8068da155134a1d72547c5f1af7d11d4bc
pedroguarderas <pedro.guarderas@webrisko.com>
2017-10-31 19:17:35 -0500

    Improving example.

--- tests/example_6.R
+++ tests/example_6.R
@@ -1,13 +1,13 @@
 
-w<-12
-nf<-30 * w
-n<-seq( 0, nf, 1 ) / w
-N<-length(n)
 
-r<-rep( 1 + 0.03 / w, N )
-s<-( 1 + 0.02 / w )^n
-P<-250 * s
-q<-pmin( 1, 0.0005 * exp( seq( 0, 7.7, length.out = N ) ) )
+nf<-30 
+n<-seq( 0, nf, 1 )
+N<-length( n )
+
+r<-rep( 1 + 0.033, N )
+s<-( 1 + 0.02 )^n
+P<-12 * 250 * s
+q<-pmin( 1, 0.0005 * exp( seq( 0, 7.6, length.out = N ) ) )
 p<-1-q
 u<--log(p)
 uu<--u
@@ -17,17 +17,15 @@ for ( i in 1:(N-1) ) {
   b<-c( b, b[i] * r[i] + P[i] * r[i] )
 }
 
-R<-1/( 1 + 0.03 / w )
-v<-R^(-n)
-V<-v * rev( cumsum( P ) ) * rev( cumprod( p ) ) -  R * v * rev( b ) * ( 1 - rev( cumprod( p ) ) )
+I<-( 1 + 0.033 )
+R<-1/I
 
-V1<-0
+V<-0
 for ( i in 1:(N-1) ) {
-  V1<-c( V1, ( V1[i] + P[i] - R * b[i+1] * q[i] ) / ( R * p[i] ) )
+  W<-( ( V[i] + P[i] ) * I - b[i+1] * q[i] ) / p[i]
+  V<-c( V, W )
 }
 
-plot( n, V, type = 'l', lwd = 2, col = 'midnightblue', panel.first = {abline( h = 0, col = 'red', lwd = 2 )} )
-points( n, V1, type = 'l', lwd = 2, col = 'olivedrab3' )
-
-
+plot( n, V, type = 'l', lwd = 2, col = 'olivedrab3', ylim = c( 0, 2e5 ),
+      panel.first = {abline( h = 0, col = 'red', lwd = 2 )} )
 
