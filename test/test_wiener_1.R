164633e3a7a2596fc31c7a4b1827abe6ecf02b84
pedroguarderas <pedro.guarderas@webrisko.com>
2022-07-21 18:53:31 -0500

    Adding test

--- test/test_wiener_1.R
+++ test/test_wiener_1.R
@@ -1,7 +1,7 @@
 library( CFINI )
 library( testthat )
 
-n <- 10000
+n <- 1e4
 t <- seq( 0, 1, length.out = n )
 d <- 2
 W <- cf_wiener( d, t )
@@ -11,3 +11,14 @@ test_that( "Checking simulation", {
 })
 
 plot( W[,1], W[,2], type = 'l' )
+
+# Test predictor-corrector method ------------------------------------------------------------------
+t <- seq( 0, 1, length.out = 10 )
+f <- function( t, y ) {
+ A <- matrix( c( 1, 0, 2, 1 ), 2, 2 )
+ return( A %*% y )
+}
+
+v0 <- c( 1, 1 )
+f( 1, v0 )
+cf_edo_solv_precor( t, v0, f, 2, 1e-2 )
