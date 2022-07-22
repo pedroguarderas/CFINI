e4d048138d81de081589efd2270f949db9ff4c48
pedroguarderas <pedro.guarderas@webrisko.com>
2022-07-22 11:31:44 -0500

    Indenting sources

--- R/cf_lattice.R
+++ R/cf_lattice.R
@@ -8,26 +8,26 @@
 #' @return A list with a tree structure of the asset evolution
 #' @author Pedro Guarderas
 #' @examples
-#' s<-0.3
-#' T<-0.25
-#' N<-15
-#' u<-exp( s * sqrt( T / N ) )
-#' d<-1/u
-#' r<-0.02
-#' S0<-100
-#' R<-exp( r * T / N )
-#' q<-( R - d ) / ( u - d )
-#' U<-c( d, u )
-#' S<-CFLattice( N, U, S0 )
+#' s <- 0.3
+#' T <- 0.25
+#' N <- 15
+#' u <- exp( s * sqrt( T / N ) )
+#' d <- 1/u
+#' r <- 0.02
+#' S0 <- 100
+#' R <- exp( r * T / N )
+#' q <- ( R - d ) / ( u - d )
+#' U <- c( d, u )
+#' S <- CFLattice( N, U, S0 )
 #' @export
-cf_lattice<-function( N, U, S0 ) {
-  S<-list( S0, U * S0 )
-  n<-length( U )
+cf_lattice <- function( N, U, S0 ) {
+  S <- list( S0, U * S0 )
+  n <- length( U )
   
   for ( t in 3:(N+1) ) {
-    M<-n + (t-2)*(n-1)
-    K<-M - n + 1
-    S[[t]]<-c( U * S[[t-1]][1], U[n] * S[[t-1]][2:K] )
+    M <- n + (t-2)*(n-1)
+    K <- M - n + 1
+    S[[t]] <- c( U * S[[t-1]][1], U[n] * S[[t-1]][2:K] )
   }
   return( S )
 }
--- R/cf_lattice_princing.R
+++ R/cf_lattice_princing.R
@@ -6,117 +6,117 @@
 #' @param R term structure of the interest rate, could be a fixed value or a multinomial lattice
 #' @param S multinomial lattice
 #' @param option function defining the option over S
-#' @param Type option type a character that specifies the king of option, by default 'E' european
+#' @param type option type a character that specifies the king of option, by default 'E' european
 #' option, 'A' american option, 'F' futures option, 'S' swap option, 'P' ...
 #' @return A list with a tree structure of the asset evolution
 #' @author Pedro Guarderas
 #' @seealso \code{\link{CFLattice}}
 #' @examples
-#' s<-0.3
-#' T<-0.25
-#' N<-15
-#' c<-0.01
-#' u<-exp( s * sqrt( T / N ) )
-#' d<-1/u
-#' r<-0.02
-#' S0<-100
-#' K<-110
-#' R<-exp( r * T / N )
-#' q<-( R - d ) / ( u - d )
-#' Q<-c( 1-q, q )
-#' U<-c( d, u )
+#' s <- 0.3
+#' T <- 0.25
+#' N <- 15
+#' c <- 0.01
+#' u <- exp( s * sqrt( T / N ) )
+#' d <- 1/u
+#' r <- 0.02
+#' S0 <- 100
+#' K <- 110
+#' R <- exp( r * T / N )
+#' q <- ( R - d ) / ( u - d )
+#' Q <- c( 1-q, q )
+#' U <- c( d, u )
 #' 
 #' # K is global
-#' call<-function( S ) {
+#' call <- function( S ) {
 #'   max( S - K, 0 )
 #' }
 #' 
-#' put<-function( S ) {
+#' put <- function( S ) {
 #'   max( K - S, 0 )
 #' }
 #'
 #' # Equivalent measure
-#' EQ<-function(R,Q,C) {
+#' EQ <- function(R,Q,C) {
 #'   return( sum( R * Q * C ) )
 #' }
 #' 
-#' S<-CFLattice( N, U, S0 )
+#' S <- CFLattice( N, U, S0 )
 #' 
 #' # Pricing american call
-#' Ca<-CFLatticePricing( Q, EQ, R, S, call, Type = 'A' )
+#' Ca <- CFLatticePricing( Q, EQ, R, S, call, type = 'A' )
 #' 
 #' # Pricing american put
-#' Pa<-CFLatticePricing( Q, EQ, R, S, put, Type = 'A' )
+#' Pa <- CFLatticePricing( Q, EQ, R, S, put, type = 'A' )
 #' @export
-cf_lattice_pricing<-function( Q, EQ, R, S, option, Type = 'E' ) {
-  C<-S
-  N<-length(S)
-  n<-length(Q)
+cf_lattice_pricing <- function( Q, EQ, R, S, option, type = 'E' ) {
+  C <- S
+  N <- length(S)
+  n <- length(Q)
   
-  check<-1
+  check <- 1
   if ( length( R ) > 1 ) {
-    ns<-unlist( lapply( S, FUN = length ) )
-    nr<-unlist( lapply( R, FUN = length ) )
+    ns <- unlist( lapply( S, FUN = length ) )
+    nr <- unlist( lapply( R, FUN = length ) )
     if ( length( ns ) < length(nr) ) {
-      if ( all( ns == nr[1:length(ns)] ) ) check<-2
+      if ( all( ns == nr[1:length(ns)] ) ) check <- 2
     } else if ( length( ns ) == length(nr) ) {
-      if ( all( ns == nr ) ) check<-2
+      if ( all( ns == nr ) ) check <- 2
     } else if ( length( ns ) > ( length(nr) + 1 ) ) {
-      if ( all( ns[1:length(nr)] == nr ) ) check<-2
+      if ( all( ns[1:length(nr)] == nr ) ) check <- 2
     }
   }
   
-  if ( Type == 'E' & check == 1 ) {
-    C[[N]]<-sapply( C[[N]], FUN = option )
+  if ( type == 'E' & check == 1 ) {
+    C[[N]] <- sapply( C[[N]], FUN = option )
     for ( t in (N-1):1 ) {
-      M<-n + (t-2)*(n-1)
-      C[[t]]<-sapply( 1:M, FUN = function(k) EQ( 1/R, Q, C[[t+1]][k:(k+n-1)] )  )
+      M <- n + (t-2)*(n-1)
+      C[[t]] <- sapply( 1:M, FUN = function(k) EQ( 1/R, Q, C[[t+1]][k:(k+n-1)] )  )
     }
-  } else if ( Type == 'E' & check == 2 ) {
-    C[[N]]<-sapply( C[[N]], FUN = option )
+  } else if ( type == 'E' & check == 2 ) {
+    C[[N]] <- sapply( C[[N]], FUN = option )
     for ( t in (N-1):1 ) {
-      M<-n + (t-2)*(n-1)
-      C[[t]]<-sapply( 1:M, FUN = function(k) EQ( 1 / ( 1 + R[[t]][k] ), Q, C[[t+1]][k:(k+n-1)] )  )
+      M <- n + (t-2)*(n-1)
+      C[[t]] <- sapply( 1:M, FUN = function(k) EQ( 1 / ( 1 + R[[t]][k] ), Q, C[[t+1]][k:(k+n-1)] )  )
     }
-  } else if ( Type == 'A' & check == 1 ) {
-    C[[N]]<-sapply( C[[N]], FUN = option )
+  } else if ( type == 'A' & check == 1 ) {
+    C[[N]] <- sapply( C[[N]], FUN = option )
     for ( t in (N-1):1 ) {
-      M<-n + (t-2)*(n-1)
-      C[[t]]<-sapply( 1:M, FUN = function(k) max( option( S[[t]][k] ),
+      M <- n + (t-2)*(n-1)
+      C[[t]] <- sapply( 1:M, FUN = function(k) max( option( S[[t]][k] ),
                                                   EQ( 1/R, Q, C[[t+1]][k:(k+n-1)] ) ) )
     }
-  } else if ( Type == 'A' & check == 2 ) {
-    C[[N]]<-sapply( C[[N]], FUN = option )
+  } else if ( type == 'A' & check == 2 ) {
+    C[[N]] <- sapply( C[[N]], FUN = option )
     for ( t in (N-1):1 ) {
-      M<-n + (t-2)*(n-1)
-      C[[t]]<-sapply( 1:M, FUN = function(k) max( option( S[[t]][k] ),
+      M <- n + (t-2)*(n-1)
+      C[[t]] <- sapply( 1:M, FUN = function(k) max( option( S[[t]][k] ),
                                                   EQ( 1 / ( 1 + R[[t]][k] ), Q, C[[t+1]][k:(k+n-1)] ) ) )
     }
-  } else if ( Type == 'F' & check == 1 ) {
+  } else if ( type == 'F' & check == 1 ) {
     for ( t in (N-1):1 ) {
-      M<-n + (t-2)*(n-1)
-      C[[t]]<-sapply( 1:M, FUN = function(k) EQ( 1 / R, Q, C[[t+1]][k:(k+n-1)] )  )
+      M <- n + (t-2)*(n-1)
+      C[[t]] <- sapply( 1:M, FUN = function(k) EQ( 1 / R, Q, C[[t+1]][k:(k+n-1)] )  )
     }
-  } else if ( Type == 'F' & check == 2 ) {
+  } else if ( type == 'F' & check == 2 ) {
     for ( t in (N-1):1 ) {
-      M<-n + (t-2)*(n-1)
-      C[[t]]<-sapply( 1:M, FUN = function(k) EQ( 1 / ( 1 + R[[t]][k] ), Q, C[[t+1]][k:(k+n-1)] )  )
+      M <- n + (t-2)*(n-1)
+      C[[t]] <- sapply( 1:M, FUN = function(k) EQ( 1 / ( 1 + R[[t]][k] ), Q, C[[t+1]][k:(k+n-1)] )  )
     }
-  } else if ( Type == 'S' & check == 2 ) {
-    C[[N]]<-sapply( C[[N]], FUN = option )
-    C[[N]]<-C[[N]] / ( 1 + R[[N]] )
+  } else if ( type == 'S' & check == 2 ) {
+    C[[N]] <- sapply( C[[N]], FUN = option )
+    C[[N]] <- C[[N]] / ( 1 + R[[N]] )
     for ( t in (N-1):1 ) {
-      M<-n + (t-2)*(n-1)
-      C[[t]]<-sapply( 1:M, FUN = function(k) {
+      M <- n + (t-2)*(n-1)
+      C[[t]] <- sapply( 1:M, FUN = function(k) {
         EQ( 1 / ( 1 + R[[t]][k] ), Q, C[[t+1]][k:(k+n-1)] ) +
           option( S[[t]][k] ) / ( 1 + R[[t]][k] )
       } )
     }
-  } # else if ( Type = 'P' & check == 2 ) {
-  #   C[[1]]<-1
+  } # else if ( type = 'P' & check == 2 ) {
+  #   C[[1]] <- 1
   #   for ( t in 2:N ) {
-  #     M<-n + (t-2)*(n-1)
-  #     C[[t]]<-sapply( 1:M, FUN = function(k) {
+  #     M <- n + (t-2)*(n-1)
+  #     C[[t]] <- sapply( 1:M, FUN = function(k) {
   #       EQ( 1 / ( 1 + R[[t]][k] ), Q[], C[[t-1]][k:(k+n-1)] )
   #       
   #     } )
