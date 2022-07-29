02c00e7d47244045cac5ca75540a540c1ab47d93
pedroguarderas <pedro.guarderas@webrisko.com>
2022-07-29 18:06:13 -0500

    Indenting text

--- src/cf_wiener.h
+++ src/cf_wiener.h
@@ -22,29 +22,29 @@ using namespace Rcpp;
 //' @export
 // [[Rcpp::export]]
 Eigen::MatrixXd cf_wiener( const int& d,
-                             const Eigen::VectorXd& t );
+                           const Eigen::VectorXd& t );
 
 //--------------------------------------------------------------------------------------------------
 /*
-//' @title Brownian motion
-//' @description Simulate d-dimensional Brownian motion
-//' @param d1
-//' @param d2
-//' @param X0
-//' @param b
-//' @param s
-//' @param t
-//' @return Solution to the stochastic differential equation
-//' @note Diffusion solver for pricing options
-//' @author Pedro Guarderas
-//' @export
-// [[Rcpp::export]]
-Eigen::MatrixXd cf_stoch_solv( const int& d1,
-                               const int& d2,
-                               const Eigen::VectorXd& X0,
-                               const Function& b,
-                               const Function& s,
-                               const Eigen::VectorXd& t );
-
-*/
+ //' @title Brownian motion
+ //' @description Simulate d-dimensional Brownian motion
+ //' @param d1
+ //' @param d2
+ //' @param X0
+ //' @param b
+ //' @param s
+ //' @param t
+ //' @return Solution to the stochastic differential equation
+ //' @note Diffusion solver for pricing options
+ //' @author Pedro Guarderas
+ //' @export
+ // [[Rcpp::export]]
+ Eigen::MatrixXd cf_stoch_solv( const int& d1,
+ const int& d2,
+ const Eigen::VectorXd& X0,
+ const Function& b,
+ const Function& s,
+ const Eigen::VectorXd& t );
+ 
+ */
 #endif
