811701777a3976903ba8e036b229c4213f753ba2
pedroguarderas <pedro.guarderas@webrisko.com>
2022-07-29 16:50:24 -0500

    Improving test

--- tests/testthat/test_cf_trid_diag_solv.R
+++ tests/testthat/test_cf_trid_diag_solv.R
@@ -1,6 +1,6 @@
 library( testthat )
 
-d <- 100
+d <- 50
 A <- diag( rep( -2, d ), d )
 for ( i in 1:( d - 1 ) ) {
   A[ i, i + 1 ] <- 1
@@ -8,15 +8,15 @@ for ( i in 1:( d - 1 ) ) {
 }
 
 b <- rep( 1, d )
-c <- rep( -1e3, d )
+c <- rep( -1e4, d )
 u0 <- runif( d )
-n <- 100
-w <- 0.95
-e <- 1e-5
+n <- 100000
+w <- 0.8
+e <- 1e-6
 
 S <- cf_psor_solv( u0, A, b, c, w, n, e ) 
 u <- S$u
 
 test_that( "Checking numerical solution with PSOR algorithm", {
-  expect_lt( norm( A %*% u - b, type = '2' ), 0.0001 )
+  expect_lt( norm( A %*% u - b, type = '2' ), 1e-6 )
 })
