e6b96373811150e75e65a5456ba5d7cc9106d17a
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2023-10-31 15:11:52 -0500

    Include description

--- R/CFINI.R
+++ R/CFINI.R
@@ -1,2 +1,17 @@
+# CFINI --------------------------------------------------------------------------------------------
+#' @title CFINI
+#' @description This package was created with implement different computational routines that can 
+#' be applied to solve pricig problems, that are usually presented in Finance and Actuarial Science.
+#' 
+#' Here a list of the main numerical methods implemented inside \pkg{CFINI}. Many of this functions 
+#' are coded with C++ by employing the packages Rcpp and RcppEigen.
+#' \enumerate{
+#'   \item Pricing with trees.
+#'   \item Pricing with multinomial trees.
+#'   \item Ordinary differential equation solver implemented with the predictor-corrector method.
+#'   \item Diffusion solver with Euler implicit scheme.
+#'   \item Diffusion solver with Crank-Nicolson scheme.
+#' }
 #' @useDynLib CFINI, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
+"_PACKAGE"
