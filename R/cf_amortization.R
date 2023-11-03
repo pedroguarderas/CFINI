c4bceeefda6d805c9425a17080c782d6aba93215
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2023-11-03 17:50:03 -0500

    Update function

--- R/cf_amortization.R
+++ R/cf_amortization.R
@@ -8,29 +8,32 @@
 #' @return A data.table with amortization information
 #' @author Pedro Guarderas
 #' @examples
-#' r<-0.16
-#' y<-10
-#' p<-12
-#' M<-10000
-#' A<-cf_amortization( r, y, p, M )
+#' r <- 0.16
+#' y <- 10
+#' p <- 12
+#' M <- 10000
+#' A <- cf_amortization( r, y, p, M )
 #' sum( A$c )
 #' sum( A$i )
 #' @importFrom data.table := data.table
 #' @export
-cf_amortization<-function( r, y, p, M ) {
+cf_amortization <- function( r, y, p, M ) {
   
-  I <- r/p
+  I <- r / p
   N <- y * p
-  R <- -( 1 - ( 1 + I )^( N + 1 ) )/ I
+  R <- -( 1 - ( 1 + I )^( N + 1 ) ) / I
   C <- M * ( I * ( 1 + I )^N ) / ( ( 1 + I )^N - 1)
   
-  A <- data.table( t = seq( 0, N-1, 1 ) )
-  A[ , y := t / p ]
-  A[ , r := ( 1 + I )^t ]
-  A[ , v := 1/r ]
-  A[ , m := M * ( 1 + I )^t  + C * ( 1 - ( 1 + I )^t ) / I ]
-  A[ , i := m * I ]
-  A[ , c := C - i ]
+  t <- seq( 0, N - 1, 1 )
+  m <- M * ( 1 + I )^t  + C * ( 1 - ( 1 + I )^t ) / I
+  A <- data.table( 
+    t = t, 
+    y = t / p,
+    r = ( 1 + I )^t,
+    v = ( 1 + I )^(-t),
+    m = m,
+    i = m * I,
+    c = C - m * I )
   
   return( A )
   
