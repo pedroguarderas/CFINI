fff3aab31acde6bfd16d5fbf205e7c1443bc6265
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2023-11-03 02:31:04 -0500

    Adding new methods

--- R/cf_lattice_class.R
+++ R/cf_lattice_class.R
@@ -1,7 +1,32 @@
-setClass( 'cflattice', representation( lattice = 'list' ) )
+setClass( 'cflattice', representation( lattice = 'list', order = 'numeric' ) )
 
-setMethod( "[", "cflattice",
-           function( x, i ) {
-             return( x@lattice[[ i ]] )
-           }
+setMethod( 
+  "[", 
+  "cflattice",
+  function( x, i ) {
+    return( x@lattice[[ i ]] )
+  }
+)
+
+setMethod( 
+  "length", 
+  "cflattice",
+  function( x ) {
+    return( length( x@lattice ) )
+  }
+)
+
+setGeneric( 
+  "order", 
+  valueClass = "numeric", 
+  function( x ) {
+  standardGeneric( "order" )
+})
+
+setMethod( 
+  "order", 
+  "cflattice",
+  function( x ) {
+    return( x@order )
+  }
 )
