7d2cbbba35a9f1485784fb064ecbeb83f31e85dd
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2023-11-03 01:58:25 -0500

    Indenting sources.

--- R/cf_lattice.R
+++ R/cf_lattice.R
@@ -9,26 +9,26 @@
 #' @author Pedro Guarderas
 #' \email{pedro.felipe.guarderas@@gmail.com}
 #' @examples
-# s <- 0.3
-# T <- 0.25
-# N <- 15
-# u <- exp( s * sqrt( T / N ) )
-# d <- 1/u
-# r <- 0.02
-# S0 <- 100
-# R <- exp( r * T / N )
-# q <- ( R - d ) / ( u - d )
-# U <- c( d, u )
-# S <- cf_lattice( N, U, S0 )
+#' s <- 0.3
+#' T <- 0.25
+#' N <- 15
+#' u <- exp( s * sqrt( T / N ) )
+#' d <- 1/u
+#' r <- 0.02
+#' S0 <- 100
+#' R <- exp( r * T / N )
+#' q <- ( R - d ) / ( u - d )
+#' U <- c( d, u )
+#' S <- cf_lattice( N, U, S0 )
 #' @export
 cf_lattice <- function( N, U, S0 ) {
   S <- list( S0, U * S0 )
   n <- length( U )
   
-  for ( t in 3:(N+1) ) {
-    M <- n + (t-2)*(n-1)
+  for ( t in 3:( N + 1 ) ) {
+    M <- n + ( t - 2 ) * ( n - 1 )
     K <- M - n + 1
-    S[[t]] <- c( U * S[[t-1]][1], U[n] * S[[t-1]][2:K] )
+    S[[ t ]] <- c( U * S[[ t - 1 ]][ 1 ], U[ n ] * S[[ t - 1 ]][ 2:K ] )
   }
-  return( S )
+  return( new( 'cflattice', lattice = S ) )
 }
